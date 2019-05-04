f1730fbe3a0e3d28420e487a871dd61077e30d9c
Seth <Seth.Sandor@thehartford.com>
2019-05-04 18:10:59 -0400

    loaded in Sass too

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Sample from './Sample/Sample'
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
   return (
